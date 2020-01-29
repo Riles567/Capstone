5d118e1856e55b85fedfc34e48d099b53f9e5137
Riles567 <ariles567@gmail.com>
2020-01-28 22:59:50 -0600

    making function to clean data

--- data.R
+++ data.R
@@ -42,36 +42,48 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
     arb18 <- read_excel("arb2018.xlsx")
 
 # Data Cleaning
-    
+    #Function for cleaning
+      clean <- function(arb){
+        arb <- arb[,-3]
+        arb.clean <- arb
+        arb.clean$`Settled Amt.` <- str_remove_all(arb.clean$`Settled Amt.`, "[$M]")
+        arb.clean$Midpoint <- str_remove_all(arb.clean$Midpoint, "[$M]")
+        arb.clean$`Team Amt.` <- str_remove_all(arb.clean$`Team Amt.`, "[$M]")
+        arb.clean$`Player Amt.` <- str_remove_all(arb.clean$`Team Amt.`, "[$M]")
+        arb.clean$Player <- str_remove_all(arb.clean$Player, "[??????]")
+        arb.clean$`Settled Amt.` <- as.numeric(arb.clean$`Settled Amt.`)
+        arb.clean$Midpoint <- as.numeric(arb.clean$Midpoint)
+        arb.clean$`Team Amt.` <- as.numeric(arb.clean$`Team Amt.`)
+        arb.clean$`Player Amt.` <- as.numeric(arb.clean$`Player Amt.`)
+        arb.clean <- na.omit(arb.clean)
+      }
     #arbitration data
+      #all numberic values outside of year is in millions
       #2011
-        arb11 <- arb11[,-3]
-        arb11 <- na.omit(arb11)
-        arb11.clean <- arb11
-        arb11.clean$`Settled Amt.` <- str_remove_all(arb11.clean$`Settled Amt.`,"[$M]")
-        arb11.clean$Midpoint <- str_remove_all(arb11.clean$Midpoint, "[$M]")
-        arb11.clean$`Team Amt.` <- str_remove_all(arb11.clean$`Team Amt.`, "[$M]")
-        arb11.clean$`Player Amt.` <- str_remove_all(arb11.clean$`Player Amt.`, "[$M]")
-        
+        arb11.clean <- clean(arb11)
         head(arb11.clean)
       #2012
-        arb12 <- arb12[,-3]
-        arb12 <- na.omit(arb12)
-        arb12.clean <- arb12
-        arb12.clean$`Settled Amt.` <- str_remove_all(arb12.clean$`Settled Amt.`, "[$M]")
-        arb12.clean$Midpoint <- str_remove_all(arb12.clean$Midpoint, "[$M]")
-        arb12.clean$`Team Amt.` <- str_remove_all(arb12.clean$`Team Amt.`, "[$M]")
-        arb12.clean$`Player Amt.` <- str_remove_all(arb12.clean$`Player Amt.`, "[$M]")
+        arb12.clean <- clean(arb12)
         head(arb12.clean)
       #2013
-        
+        arb13.clean <- clean(arb13)
+        head(arb13.clean)
       #2014
+        arb14.clean <- clean(arb14)  
+        head(arb14.clean)
       #2015
+        arb15.clean <- clean(arb15)
+        head(arb15.clean)
       #2016
+        arb16.clean <- clean(arb16)
+        head(arb16.clean)
       #2017
+        arb17.clean <- clean(arb17)
+        head(arb17.clean)
       #2018
-     
-      
+        arb18.clean <- clean(arb18)
+        arb18.clean$Player <- str_remove_all(arb18.clean$Player, "[??????]")
+        head(arb18.clean)
 # Data Calculations
   
 # Data Joining
