73a7676457f2ff2ccebc9ab0b14462ff744450e5
Riles <yz9186ci@winona.edu>
2020-02-13 11:58:12 -0600

    finish SLG and RC data and Stacked WAR and Arbitration data

--- data.R
+++ data.R
@@ -13,24 +13,30 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
 
     player <- read.csv("People.csv")
     salary <- read.csv("Salaries.csv")
-    names(field)
   
   #WAR
     
     war11 <- read.csv("FanGraphs Leaderboard2011.csv")
+    war11$Year <- 2011
     war12 <- read.csv("FanGraphs Leaderboard2012.csv")
+    war12$Year <- 2012
     war13 <- read.csv("FanGraphs Leaderboard2013.csv")
+    war13$Year <- 2013
     war14 <- read.csv("FanGraphs Leaderboard2014.csv")
+    war14$Year <- 2014
     war15 <- read.csv("FanGraphs Leaderboard2015.csv")
+    war15$Year <- 2015
     war16 <- read.csv("FanGraphs Leaderboard2016.csv")
+    war16$Year <- 2016
     war17 <- read.csv("FanGraphs Leaderboard2017.csv")
+    war17$Year <- 2017
     war18 <- read.csv("FanGraphs Leaderboard2018.csv")
-    head(war11)
+    war18$Year <- 2018
   #Fielding Independent Pitching (FIP)
     
     FIPcon <- read.csv("FanGraphs LeaderboardFIP.csv")
     pitching <- read.csv("Pitching.csv")
-  names(FIPcon)
+
   #Stats for Calculations
     
     team <- read.csv("Teams.csv")
@@ -49,7 +55,6 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
     arb17 <- read_excel("arb2017.xlsx")
     arb18 <- read_excel("arb2018.xlsx")
     
-    head(arb11)
   #Defensice Runs Saved
     
     drs11 <- read.csv("DRS2011.csv")
@@ -60,9 +65,12 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
     drs16 <- read.csv("DRS2016.csv")
     drs17 <- read.csv("DRS2017.csv")
     drs18 <- read.csv("DRS2018.csv")
+
 # Data Cleaning and Calculations
     
     #Function for Cleaning Arbitration Data
+    #for cleaning to work when pulling off GitHub the file must be saved with encoding
+    #the encoding that it should be saved with is UTF-8
       clean <- function(arb){
         arb <- arb[,-3]
         arb.clean <- arb
@@ -84,28 +92,20 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
       
       #2011
         arb11.clean <- clean(arb11)
-        head(arb11.clean)
       #2012
         arb12.clean <- clean(arb12)
-        head(arb12.clean)
       #2013
         arb13.clean <- clean(arb13)
-        head(arb13.clean)
       #2014
         arb14.clean <- clean(arb14)  
-        head(arb14.clean)
       #2015
         arb15.clean <- clean(arb15)
-        head(arb15.clean)
       #2016
         arb16.clean <- clean(arb16)
-        head(arb16.clean)
       #2017
         arb17.clean <- clean(arb17)
-        head(arb17.clean)
       #2018
         arb18.clean <- clean(arb18)
-        head(arb18.clean)
     
     #DRS Data
         
@@ -146,25 +146,20 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
           warpit18 <- pit(war18)
   
   #Fielding Independent Pitching and Other predictors for Pitchers
-      names(player)
-      names(pitching)
-      names(FIPcon)
-      head(FIPcon)
+      
       FIPcon <- select(FIPcon, ï..Season, cFIP)
       
       #Formula (13 * HR + 3*(BB + HBP) - 2*K)/(IP) + FIP COnstant
         pitchers <- left_join(player, pitching, by = c("playerID" = "playerID"), copy = FALSE)
         pitchers <- unite(pitchers, Player, c(nameFirst, nameLast), sep = " ")
-        pitchers <- select(pitchers, nameFirst, nameLast, yearID, IPouts, HR, SO, BB, IBB, HBP)
+        pitchers <- select(pitchers, Player, yearID, G,IPouts, ERA, HR, H, SO, BB, IBB, HBP)
         pitchers <- left_join(pitchers, FIPcon, by = c("yearID" = "ï..Season"), copy = FALSE)
         pitchers <- filter(pitchers, yearID >= 2011, IPouts >= 30, G >= 5)
         pitchers$FIP <- ((13*pitchers$HR + 3*(pitchers$BB + pitchers$IBB + pitchers$HBP) - 2*pitchers$SO)/(pitchers$IPouts/3))+pitchers$cFIP
         pitchers$IP <- pitchers$IPouts/3
-        pitchers$WHIP <- (pitchers$BB+  pitchers$HBP + pitchers$IBB + pitchers$H)/pitchers$IP
-        pitchers <- select(pitchers, playerID, Player, IP, ERA, FIP, SO, WHIP)
-        
-  #OPS100
-  
+        pitchers$WHIP <- (pitchers$BB +  pitchers$HBP + pitchers$IBB + pitchers$H)/pitchers$IP
+        pitchers <- select(pitchers, Player, yearID, IP, ERA, FIP, SO, WHIP)
+
   #Predictors for position players
       # hitting
         hitfield <- select(field, playerID, POS, yearID)
@@ -172,19 +167,71 @@ setwd("C:/Users/arile/Desktop/Capstone/DATA") #desktop
         hit <- unite(hit, playerName, c(nameFirst, nameLast), sep = " ", remove = FALSE)
         hit <- select(hit, playerID, playerName, yearID, G, AB, R, H, X2B, X3B, HR, RBI, SB, CS, BB, SO, IBB, HBP, GIDP, SF, SH)
         hit <- filter(hit, yearID >= "2011", G >= 10, AB >= 50)
-        hit$SLG <- (hit$H + (2*hit$x2B) + (3*hit$x3B) + (4*hit$HR))/hit$AB
+        hit$SLG <- (hit$H + (2*hit$X2B) + (3*hit$X3B) + (4*hit$HR))/hit$AB
         hit$AVG <- (hit$H + hit$X2B + hit$X3B + hit$HR)/hit$AB
         hit$OBP <- (hit$H + hit$BB + hit$IBB + hit$HBP)/(hit$AB + hit$BB + hit$IBB + hit$HBP + hit$SF + hit$SH)
         hit$OPS <- hit$SLG + hit$OBP
         hit <- group_by(hit, yearID)
         hit$lgOBP <- mean(hit$OBP)
         hit$lgSLG <- mean(hit$SLG)
-        hit$OPSplus <- 100 * ((hit$OBP/hit$lgOBP) + (hit$SLG/hit$lgSLG) - 1)/
+        hit$OPSplus <- 100 * ((hit$OBP/hit$lgOBP) + (hit$SLG/hit$lgSLG) - 1)/1
+        hit$RC <- (hit$H + hit$BB - hit$CS + hit$HBP - hit$GIDP)*((hit$H + (2*hit$X2B) + (3*hit$X3B) + 
+                      (4*hit$HR)+(.26*(hit$BB - hit$IBB + hit$HBP))+(.52*(hit$SH + hit$SF + hit$SB))))/(hit$AB + hit$BB + hit$HBP + hit$SH + hit$SF)
+      
         summary(hit)
         head(hit)
         View(hit)
+        names(hit)
       #fielding
 # Data Joining
   
-  #Stacking the arbitration data into one table
+  #Combining War and Arbitration data sets
+
+    #pitching WAR data
+      arbwarpit11 <- left_join(arb11.clean, warpit11, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit12 <- left_join(arb12.clean, warpit12, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit13 <- left_join(arb13.clean, warpit13, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit14 <- left_join(arb14.clean, warpit14, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit15 <- left_join(arb15.clean, warpit15, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit16 <- left_join(arb16.clean, warpit16, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit17 <- left_join(arb17.clean, warpit17, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpit18 <- left_join(arb18.clean, warpit18, by = c("Player" = "ï..Name"), copy = FALSE)
     
+    #posistion players
+      arbwarpos11 <- left_join(arb11.clean, warpos11, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos12 <- left_join(arb12.clean, warpos12, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos13 <- left_join(arb13.clean, warpos13, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos14 <- left_join(arb14.clean, warpos14, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos15 <- left_join(arb15.clean, warpos15, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos16 <- left_join(arb16.clean, warpos16, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos17 <- left_join(arb17.clean, warpos17, by = c("Player" = "ï..Name"), copy = FALSE)
+      arbwarpos18 <- left_join(arb18.clean, warpos18, by = c("Player" = "ï..Name"), copy = FALSE)
+
+  #WAR pitchers
+    arbwarpit <- Stack(arbwarpit11, arbwarpit12)
+    arbwarpit <- Stack(arbwarpit, arbwarpit13)
+    arbwarpit <- Stack(arbwarpit, arbwarpit14)
+    arbwarpit <- Stack(arbwarpit, arbwarpit15)
+    arbwarpit <- Stack(arbwarpit, arbwarpit16)
+    arbwarpit <- Stack(arbwarpit, arbwarpit17)
+    arbwarpit <- Stack(arbwarpit, arbwarpit18)
+    arbwarpit <- arbwarpit[,-11]
+    arbwarpit <- na.omit(arbwarpit)
+    head(arbwarpit)
+  #WAR Position players
+    arbwarpos <- Stack(arbwarpos11, arbwarpos12)
+    arbwarpos <- Stack(arbwarpos, arbwarpos13)
+    arbwarpos <- Stack(arbwarpos, arbwarpos14)
+    arbwarpos <- Stack(arbwarpos, arbwarpos15)
+    arbwarpos <- Stack(arbwarpos, arbwarpos16)
+    arbwarpos <- Stack(arbwarpos, arbwarpos17)
+    arbwarpos <- Stack(arbwarpos, arbwarpos18)
+    arbwarpos <- arbwarpos[,-12]
+    arbwarpos <- na.omit(arbwarpos)
+    arbwarpos <- select()
+    
+    View(arbwarpos)
+    head(arbwarpos)
+  #combining arbwar data with pitching data
+  
+  #combining arbwar data with position players data
